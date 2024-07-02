b2570ebe6afb5d913050126ff482299def0b4970
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-07-02 20:28:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,67 +11,76 @@ public class Main {
             System.out.println("Welcome to cheese shop! Please select access type: ");
             System.out.println("1 - admin;");
             System.out.println("2 - customer;");
+            System.out.println("3 - exit.");
 
             var accessType = scanner.nextLine();
 
             if (accessType.equals("1")) {
-                System.out.println("Which actions you want to do?");
-                System.out.println("1 - add item;");
-                System.out.println("2 - update item;");
-                System.out.println("3 - remove item;");
-                System.out.println("4 - change access type;");
-                System.out.println("5 - exit;");
+                // Admin actions loop
+                while (true) {
+                    System.out.println("Please choose an action:");
+                    System.out.println("1 - add item;");
+                    System.out.println("2 - update item;");
+                    System.out.println("3 - remove item;");
+                    System.out.println("4 - change access type;");
+                    System.out.println("5 - exit.");
 
-                var adminAction = scanner.nextLine();
+                    var adminAction = scanner.nextLine();
 
-                if (adminAction.equals("1")) {
-                    addCheese();
-                    printCheese();
-                } else if (adminAction.equals("2")) {
-                    updateCheese();
-                    printCheese();
-                } else if (adminAction.equals("3")) {
-                    System.out.println("Items ID: ");
-                    int id = scanner.nextInt();
-                    scanner.nextLine();
-                    cheeseService.removeCheese(id);
-                    printCheese();
-                } else if (adminAction.equals("4")) {
-                    continue;
-                } else if (adminAction.equals("5")) {
-                    System.out.println("See you next time!");
-                    break;
+                    if (adminAction.equals("1")) {
+                        addCheese();
+                        printCheese();
+                    } else if (adminAction.equals("2")) {
+                        updateCheese();
+                        printCheese();
+                    } else if (adminAction.equals("3")) {
+                        System.out.println("Items ID: ");
+                        int id = scanner.nextInt();
+                        scanner.nextLine();
+                        cheeseService.removeCheese(id);
+                        printCheese();
+                    } else if (adminAction.equals("4")) {
+                        break; // Exit admin actions loop to return to access type selection
+                    } else if (adminAction.equals("5")) {
+                        System.out.println("See you next time!");
+                        return; // Exit the entire program
+                    }
                 }
 
             } else if (accessType.equals("2")) {
-                System.out.println("Which actions you want to do?");
-                System.out.println("1 - add item to cart;");
-                System.out.println("2 - remove item from cart;");
-                System.out.println("3 - show cart;");
-                System.out.println("4 - change access type;");
-                System.out.println("5 - checkout;");
-                System.out.println("6 - register new customer;");
+                // Customer actions loop
+                while (true) {
+                    System.out.println("Please choose an action:");
+                    System.out.println("1 - add item to cart;");
+                    System.out.println("2 - remove item from cart;");
+                    System.out.println("3 - show cart;");
+                    System.out.println("4 - change access type;");
+                    System.out.println("5 - checkout;");
+                    System.out.println("6 - register new customer.");
 
-                var customerAction = scanner.nextLine();
+                    var customerAction = scanner.nextLine();
 
-                if (customerAction.equals("1")) {
-                    addCheeseToCart();
-                    showCart();
-                } else if (customerAction.equals("2")) {
-                    removeCheeseToCart();
-                    showCart();
-                } else if (customerAction.equals("3")) {
-                    showCart();
-                } else if (customerAction.equals("4")) {
-                    continue;
-                } else if (customerAction.equals("5")) {
-                    System.out.println("Add your name:");
-                    String name = scanner.nextLine();
-                    break;
-                } else if (customerAction.equals("6")) {
-                    addCustomer();
+                    if (customerAction.equals("1")) {
+                        addCheeseToCart();
+                        showCart();
+                    } else if (customerAction.equals("2")) {
+                        removeCheeseToCart();
+                        showCart();
+                    } else if (customerAction.equals("3")) {
+                        showCart();
+                    } else if (customerAction.equals("4")) {
+                        break; // Exit customer actions loop to return to access type selection
+                    } else if (customerAction.equals("5")) {
+                        System.out.println("Add your name:");
+                        String name = scanner.nextLine();
+                        int totalAmount = cheeseShop.checkout(name);
+                        break; // Exit the customer actions loop
+                    } else if (customerAction.equals("6")) {
+                        addCustomer();
+                    }
                 }
-                
+            } else if (accessType.equals("3")) {
+                break;
             }
         }
     }
